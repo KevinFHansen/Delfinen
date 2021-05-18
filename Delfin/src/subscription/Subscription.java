4caf4e4f0715d9629c5081037ac50b232de2cb36
Lars <larsbrogaard86@gmail.com>
2021-05-18 17:02:07 +0200

    Lars

--- Delfin/src/subscription/Subscription.java
+++ Delfin/src/subscription/Subscription.java
@@ -1,10 +1,13 @@
 package subscription;
 
+import members.Member;
+
 public class Subscription {
     private static String typeOfSub;
     private static boolean isActive;
     private static boolean isSenior;
     private static boolean isSixtyPlus;
+    private static Member member;
 
     public Subscription(String typeOfSub, boolean isActive, boolean isSenior, boolean isSixtyPlus){
 
@@ -12,6 +15,19 @@ public class Subscription {
     }
 
 
-    public static double generatePrice(Member member)
+    public static double generatePrice(Member member){
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 }
