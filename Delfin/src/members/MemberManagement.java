8b454e0069c80f1ca01bed98b99ecd98fd49fcb6
KevinFHansen <kevi4777@stud.kea.dk>
2021-05-18 14:05:45 +0200

    Tilføjet ufærdige metoder til MemberManagement

--- Delfin/src/members/MemberManagement.java
+++ Delfin/src/members/MemberManagement.java
@@ -1,4 +1,23 @@
 package members;
 
+import java.util.ArrayList;
+
 public class MemberManagement {
+
+    Member member = new Member;
+    ArrayList<Member> memberlist = new ArrayList<Member>();
+
+    //Skal kunne oprette et nyt medlem og registrere data - Gemme members i en arraylist
+    public static void registerMember(){
+    }
+
+    public static void updateMember(){
+    }
+
+    public static void findMember(){
+    }
+
+    public static void removeMember(){
+    }
+
 }
