adbd8eb8ef76491bd98b7d182b7bc0a58334d127
KevinFHansen <kevi4777@stud.kea.dk>
2021-05-19 17:00:11 +0200

    Membermanagement update

--- Delfin/src/members/MemberManagement.java
+++ Delfin/src/members/MemberManagement.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class MemberManagement {
 
-    Member member = new Member(null, 0, null,0,null, null, false);
+    //Member member = new Member(null, 0, null,0,null, null, true, true);
     private static ArrayList<Member> memberList = new ArrayList<>();
     static Scanner userInput = new Scanner(System.in);
 
@@ -29,22 +29,21 @@ public class MemberManagement {
         System.out.println("Please type phone number: ");
         int newMemberPhoneNumber = userInput.nextInt();
 
-        //Den her skal nok ikke indgå
         System.out.println("Please type type of subscription");
         String newMemberSubType = userInput.next();
-
-        System.out.println("Please type what type of subscription the new member wants");
-
 
         System.out.println("Please type gender");
         String newMemberGender = userInput.next();
 
-        System.out.println("Please type if member is active (True/false)");
+        System.out.println("Please type if member is active (true/false)");
         boolean isNewMemberActive = userInput.nextBoolean();
 
-        Member newMember = new Member(newMemberName, newMemberAge, newMemberEmail, newMemberPhoneNumber, newMemberSubType, newMemberGender, isNewMemberActive);
+        System.out.println("Please type if members has paid (true/false)");
+        boolean hasNewMemberPaid = userInput.nextBoolean();
+
+        Member newMember = new Member(newMemberName, newMemberAge, newMemberEmail, newMemberPhoneNumber, newMemberSubType, newMemberGender, isNewMemberActive, hasNewMemberPaid);
         memberList.add(newMember);
-        changeMemberList(memberList);
+        createMemberList(memberList);
 
     }
 
@@ -55,37 +54,41 @@ public class MemberManagement {
         System.out.println("Please type the phone number of the member you want to update");
         phoneNumber = userInput.nextInt();
         userInput.nextLine();
-        memberList.remove(phoneNumber);
+
+
         System.out.println("Please type name: ");
-        String newMemberName = userInput.next();
+        String updateMemberName = userInput.next();
 
         System.out.println("Please type age: ");
-        int newMemberAge = userInput.nextInt();
+        int updateMemberAge = userInput.nextInt();
 
         System.out.println("Please type Email: ");
-        String newMemberEmail = userInput.next();
+        String updateNewMemberEmail = userInput.next();
 
         System.out.println("Please type phone number: ");
-        int newMemberPhoneNumber = userInput.nextInt();
+        int updatePhoneNumber = userInput.nextInt();
 
         //
         System.out.println("Please type type of subscription");
-        String newMemberSubType = userInput.next();
+        String updateMemberSubType = userInput.next();
 
         System.out.println("Please type gender: ");
-        String newMemberGender =  userInput.next();
+        String updateMemberGender =  userInput.next();
 
-        System.out.println("Please type if member is active (True/false)");
-        boolean isNewMemberActive = userInput.nextBoolean();
+        System.out.println("Please type if member is active (true/false)");
+        boolean updateIsMemberActive = userInput.nextBoolean();
 
-        Member updateMember = new Member(newMemberName, newMemberAge, newMemberEmail, newMemberPhoneNumber, newMemberSubType, newMemberGender, isNewMemberActive);
+        System.out.println("Please type if the member has paid (true/false)");
+        boolean updateHasMemberPaid = userInput.nextBoolean();
+
+        Member updateMember = new Member(updateMemberName, updateMemberAge, updateNewMemberEmail, updatePhoneNumber, updateMemberSubType, updateMemberGender, updateIsMemberActive, updateHasMemberPaid);
         memberList.add(updateMember);
-        changeMemberList(memberList);
+        createMemberList(memberList);
 
 
     }
 
-    public static void findMember(int phoneNumber){
+    public static void findMember(ArrayList<Member> memberList){
     }
 
     public ArrayList<Member> readMemberList() throws FileNotFoundException {
@@ -106,30 +109,30 @@ public class MemberManagement {
             String subType = lineAsAray[4].trim();
             String gender = lineAsAray[5].trim();
             boolean isMemberActive = Boolean.parseBoolean(lineAsAray[6].trim());
-            //Vi skal have styr på constructoren i member klassen.
-            Member tmpMember = new Member(name, age, email, phoneNumber, subType, gender, isMemberActive);
+            boolean hasMemberPaid = Boolean.parseBoolean(lineAsAray[7].trim());
+            Member tmpMember = new Member(name, age, email, phoneNumber, subType, gender, isMemberActive, hasMemberPaid);
             memberList.add(tmpMember);
         }
         return memberList;
     }
 
-    public static void removeMember(int phoneNumber) throws IOException {
+    public static void removeMember(ArrayList<Member> memberList) throws IOException {
         System.out.println("Please type phone number of the member you want to remove");
-        phoneNumber = userInput.nextInt();
+        int phoneNumber = userInput.nextInt();
         for (int i = 0; i <= memberList.size(); i++) {
             Member memberToRemove = memberList.get(i);
             if(phoneNumber == memberToRemove.getPhoneNumber());
-            memberList.remove(memberList.get(i));
-            changeMemberList(memberList);
+            //memberList.remove(memberList.get(i));
+            memberList.remove(memberToRemove);
+            createMemberList(memberList);
         }
-        }
+    }
 
-
-    public static void changeMemberList(ArrayList<Member> memberList) throws FileNotFoundException {
+    public static void createMemberList(ArrayList<Member> memberList) throws FileNotFoundException {
         File fout = new File("Delfin/Ressources/Members.csv");
         PrintWriter writer = new PrintWriter(fout);
         writer.print("");
-        writer.println("name;age;email;phoneNumber;subscriptionType;gender;isMemberActive;");
+        writer.println("name;age;email;phoneNumber;subscriptionType;gender;isMemberActive;hasMemberPaid");
 
         for (int i = 0; i < memberList.size(); i++){
             writer.print(memberList.get(i).getName() + ";");
@@ -138,7 +141,9 @@ public class MemberManagement {
             writer.print(memberList.get(i).getPhoneNumber() + ";");
             writer.print(memberList.get(i).getTypeOfSubscription() + ";");
             writer.print(memberList.get(i).getGender() + ";");
-            writer.println(memberList.get(i).getIsMemberActive() + ";");
+            writer.print(memberList.get(i).getIsMemberActive() + ";");
+            writer.print(i + 1 + ";");
+            writer.println(memberList.get(i).getHasPaid() + ";");
         }
         writer.close();
     }
@@ -157,6 +162,7 @@ public class MemberManagement {
         ArrayList<Member> memberList = memberManagement.readMemberList();
         System.out.println(memberList);
         registerMember(memberList);
+        removeMember(memberList);
         System.out.println(memberList);
     }
 
