35bec519d7669eca80f7ae416e281336cfcb3215
JOBMalmstroem <jolaf3003@gmail.com>
2021-05-19 12:51:20 +0200

    payment(subIncome = done)

--- Delfin/src/subscription/Payment.java
+++ Delfin/src/subscription/Payment.java
@@ -2,6 +2,7 @@ package subscription;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Payment {
@@ -9,9 +10,38 @@ public class Payment {
     private double totalPrice;
     private int memberPhoneNumber;
 
+
+    public static void main(String[] args) throws FileNotFoundException {
+    Payment n = new Payment();
+    n.viewExpectedSubIncome();
+    }
+
     public void viewArrears() throws FileNotFoundException {
         File f = new File("Delfin/Ressources/SubscriptionList.csv");
         Scanner readCsv = new Scanner(f);
         
+    }
+    public void viewExpectedSubIncome() throws FileNotFoundException {
+        File f = new File("Delfin/Ressources/SubscriptionList.csv");
+        Scanner readCsv = new Scanner(f);
+        ArrayList<Double> n = new ArrayList<>();
+        double sum = 0;
+        while(readCsv.hasNextLine()){
+
+            String currentLine = readCsv.nextLine();
+
+            String[] csvAsArray = currentLine.split(";");
+
+            String name = csvAsArray[0];
+            int phoneNumber = Integer.parseInt(csvAsArray[1]);
+            double subIncome = Double.parseDouble(csvAsArray[2]);
+
+
+            n.add(subIncome);
+            sum = sum + subIncome;
+
+        }
+        System.out.println("This is the expected sub Income: " + sum);
+
     }
 }
