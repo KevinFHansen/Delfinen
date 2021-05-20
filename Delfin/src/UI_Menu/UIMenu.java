1654d1c665a848cd3d42f4187ee877fcbf2e40af
KevinFHansen <kevi4777@stud.kea.dk>
2021-05-20 14:50:38 +0200

    MemberManagement fixes + Added UIMenu class

--- Delfin/src/UI_Menu/UIMenu.java
+++ Delfin/src/UI_Menu/UIMenu.java
@@ -6,6 +6,7 @@ import subscription.Payment;
 import subscription.Subscription;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -21,12 +22,18 @@ public class UIMenu {
 
 
 
-    public void startUI() throws FileNotFoundException {
+    public void startUI() throws IOException {
         ArrayList<Member> memberList = memberManagement.readMemberList();
+
 
         boolean exitProgram = false;
 
         while (exitProgram){
+
+            System.out.println("1. Member Management");
+            System.out.println("2. Result Management");
+            System.out.println("3. Subscription Management");
+            System.out.println("0. Exit Program");
 
         int userInput = scn.nextInt();
 
@@ -34,23 +41,75 @@ public class UIMenu {
 
             case 1:
                 System.out.println("Member Management");
-                System.out.println("1. Register member");
-                System.out.println("2. Update member");
-                System.out.println("3. Remove member");
-                System.out.println("4. Find member");
+                System.out.println("1. Register Member");
+                System.out.println("2. Update Member");
+                System.out.println("3. Remove Member");
+                System.out.println("4. Find Member");
+                System.out.println("5. Back");
 
                 int choice = scn.nextInt();
                 if (choice == 1){
                     memberManagement.registerMember(memberList);
+
+                }
+                else if(choice == 2){
+                    memberManagement.updateMember();
+
+                }
+                else if (choice == 3){
+                    memberManagement.removeMember(memberList);
+
+                }
+                else if (choice == 4){
+                    memberManagement.findMember(memberList);
+
+                }
+                else if (choice == 5){
+
+                }
+                else {
+                    System.out.println("Wrong Input - Try again");
                 }
                 break;
 
             case 2:
                 System.out.println("Results Management");
+                System.out.println("1. Training Results");
+                System.out.println("2. Competitive Results");
+                System.out.println("3. Back");
+                int choice1 = scn.nextInt();
+
+                if (choice1 == 1) {
+                    System.out.println("Training Results");
+                    System.out.println("1. Register Result");
+                    System.out.println("2. Update Result");
+                    System.out.println("3. Delete Result");
+                    System.out.println("4. Back");
+                    int choice2 = scn.nextInt();
+                    if (choice2 == 1){
+                        //registerTrainingResult();
+                    }
+                    else if (choice2 == 2){
+                        //updateTrainingResult();
+                    }
+                    else if (choice2 == 3){
+                        //deleteTrainingResult();
+                    }
+                    else if(choice2 == 4){
+                        //Go back
+                    }
+                    else{
+                        System.out.println("Wrong input - Try again");
+                    }
+                }
                 break;
 
             case 3:
                 System.out.println("View subscriptions");
+
+            case 0:
+                System.out.println("You choose to exit");
+                exitProgram = true;
         }
 
         }
@@ -58,3 +117,4 @@ public class UIMenu {
     }
 
 }
+
