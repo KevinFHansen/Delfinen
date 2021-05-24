347a7064278c7b37c72e9f5df4a83e3f53ac78d3
MichaelDyvad <michaeldyvad@gmail.com>
2021-05-24 11:23:43 +0200

    lidt ændring i CR

---
+++ test/subscription/PaymentTest.java
@@ -0,0 +1,13 @@
+package subscription;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentTest {
+
+    @Test
+    void viewExpectedSubIncome() {
+
+    }
+}
