2032251e67de526423427057e32a01ec965f3969
MichaelDyvad <michaeldyvad@gmail.com>
2021-05-24 12:06:21 +0200

    Unit testing

--- test/subscription/PaymentTest.java
+++ test/subscription/PaymentTest.java
@@ -1,5 +1,6 @@
 package subscription;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,14 @@ class PaymentTest {
 
     @Test
     void viewExpectedSubIncome() {
+        //Arrange
+        Payment pay = new Payment();
+
+        //Act
+        double sum = pay.getSum();
+
+        //Assert
+        Assertions.assertEquals(4800.0, sum);
 
     }
 }
