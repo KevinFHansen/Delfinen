12ab8a73ae3e793622107ee46a5ce9658eeb2edb
MichaelDyvad <michaeldyvad@gmail.com>
2021-05-25 14:01:30 +0200

    payment test

--- test/subscription/PaymentTest.java
+++ test/subscription/PaymentTest.java
@@ -1,21 +1,28 @@
 package subscription;
 
+import members.Member;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentTest {
 
     @Test
-    void viewExpectedSubIncome() {
+    void viewExpectedSubIncome() throws FileNotFoundException {
         //Arrange
         Payment pay = new Payment();
 
         //Act
+        pay.viewExpectedSubIncome();
         double sum = pay.getSum();
 
         //Assert
-        Assertions.assertEquals(12600.0, sum);
+        Assertions.assertEquals(5900.0, sum);
     }
 }
