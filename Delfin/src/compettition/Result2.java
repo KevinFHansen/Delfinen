c8193688c7cab511a7e7199784dda8e90d66725f
jmunkc <jona04g8@stud.kea.dk>
2021-05-25 22:39:59 +0200

    Competitor kan læse

---
+++ Delfin/src/compettition/Result2.java
@@ -0,0 +1,55 @@
+package compettition;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Result2 {
+    String compName;
+    int placement;
+    double compTime;
+
+    public Result2(String compName, int placement, double compTime) {
+        this.compName = compName;
+        this.placement = placement;
+        this.compTime = compTime;
+    }
+
+    public String getCompName() {
+        return compName;
+    }
+
+    public void setCompName(String compName) {
+        this.compName = compName;
+    }
+
+    public int getPlacement() {
+        return placement;
+    }
+
+    public void setPlacement(int placement) {
+        this.placement = placement;
+    }
+
+    public double getCompTime() {
+        return compTime;
+    }
+
+    public void setCompTime(double compTime) {
+        this.compTime = compTime;
+    }
+
+
+    public void registerResult() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Type in competition name: ");
+        setCompName(sc.nextLine());
+
+        System.out.println("Type in placement");
+        setPlacement(sc.nextInt());
+
+        System.out.println("Type in time");
+        setCompTime(sc.nextDouble());
+
+    }
+}
